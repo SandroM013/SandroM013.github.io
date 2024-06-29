0f6411fe1dbeb6466b8b604bfbe9d45254a281e6
SandroM013 <sandro.martel013@gmail.com>
2024-06-28 20:37:29 -0500

    ChangeVite

---
+++ GenMicro_Project/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
